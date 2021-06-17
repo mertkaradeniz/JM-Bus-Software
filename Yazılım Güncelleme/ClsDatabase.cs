513bee95aa3e6b4d7771031f234e6cac4c5c8ab5
Mert Karadeniz <81584304+blackseapps@users.noreply.github.com>
2021-06-17 23:17:11 +0300

    Update ClsDatabase.cs

--- Yazılım Güncelleme/ClsDatabase.cs
+++ Yazılım Güncelleme/ClsDatabase.cs
@@ -10,6 +10,6 @@ namespace ULUSOY
 {
     class ClsDatabase
     {
-        public static string OnlineConnection = "server='93.89.230.234'; Password='jmt66382183670!';Persist Security Info=True;User ID='jmtech'; Database=jmt_db";
+        public static string OnlineConnection = "server='93.89.230.234'; Password='';Persist Security Info=True;User ID='jmtech'; Database=jmt_db";
     }
 }
